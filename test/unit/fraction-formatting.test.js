8876b74f52c0c5c874954c148d7a4997d503f436
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-21 12:19:28 +0300

    update fraction and token tests to new options API

--- test/unit/fraction-formatting.test.js
+++ test/unit/fraction-formatting.test.js
@@ -19,28 +19,35 @@ describe('Fraction Formatting', () => {
       expect(toSignificant(fractionAbove1Str)).toEqual('47619');
     });
     test('Above 1 fraction - 7 significant', () => {
-      expect(toSignificant(fractionAbove1Str, 7)).toEqual('47619.05');
+      const opts = { decimalPlaces: 7 };
+      expect(toSignificant(fractionAbove1Str, opts)).toEqual('47619.05');
     });
     test('Above 1 fraction - 10 significant', () => {
-      expect(toSignificant(fractionAbove1Str, 10)).toEqual('47619.04762');
+      const opts = { decimalPlaces: 10 };
+      expect(toSignificant(fractionAbove1Str, opts)).toEqual('47619.04762');
     });
     test('Above 1 fraction - significant default decimals - default formatting', () => {
-      expect(toSignificant(fractionAbove1Str, null, true)).toEqual('47,619');
+      const opts = { formatting: true };
+      expect(toSignificant(fractionAbove1Str, opts)).toEqual('47,619');
     });
     test('Bellow 1 fraction - significant default decimals', () => {
       expect(toSignificant(fractionBellow1Str)).toEqual('0.42857');
     });
     test('Bellow 1 fraction - 7 significant', () => {
-      expect(toSignificant(fractionBellow1Str, 7)).toEqual('0.4285714');
+      const opts = { decimalPlaces: 7 };
+      expect(toSignificant(fractionBellow1Str, opts)).toEqual('0.4285714');
     });
     test('Bellow 1 fraction - 1 significant', () => {
-      expect(toSignificant(fractionBellow1Str, 1)).toEqual('0.4');
+      const opts = { decimalPlaces: 1 };
+      expect(toSignificant(fractionBellow1Str, opts)).toEqual('0.4');
     });
     test('Bellow 1 fraction - 0 significant', () => {
-      expect(toSignificant(fractionBellow1Str, 0)).toEqual('0.42857');
+      const opts = { decimalPlaces: 0 };
+      expect(toSignificant(fractionBellow1Str, opts)).toEqual('0.42857');
     });
     test('Bellow 1 fraction - default significant decimals - default formating', () => {
-      expect(toSignificant(fractionBellow1Str, null, true)).toEqual('0.42857');
+      const opts = { formatting: true };
+      expect(toSignificant(fractionBellow1Str, opts)).toEqual('0.42857');
     });
   });
 
@@ -49,48 +56,57 @@ describe('Fraction Formatting', () => {
       expect(toFixed(fractionAbove1Str)).toEqual('47619.04762');
     });
     test('Above 1 fraction - 7 fixed', () => {
-      expect(toFixed(fractionAbove1Str, 7)).toEqual('47619.0476190');
+      const opts = { decimalPlaces: 7 };
+      expect(toFixed(fractionAbove1Str, opts)).toEqual('47619.0476190');
     });
     test('Above 1 fraction - 10 fixed', () => {
-      expect(toFixed(fractionAbove1Str, 10)).toEqual('47619.0476190476');
+      const opts = { decimalPlaces: 10 };
+      expect(toFixed(fractionAbove1Str, opts)).toEqual('47619.0476190476');
     });
     test('Above 1 fraction - default decimals - default formatting', () => {
-      expect(toFixed(fractionAbove1Str, null, true)).toEqual('47,619.04762');
+      const opts = { formatting: true };
+      expect(toFixed(fractionAbove1Str, opts)).toEqual('47,619.04762');
     });
     test('Above 1 fraction - default decimals - custom formatting', () => {
-      expect(toFixed(fractionAbove1Str, null, ['en-US'])).toEqual('47,619.048');
+      const opts = { formatting: ['en-US'] };
+      expect(toFixed(fractionAbove1Str, opts)).toEqual('47,619.048');
     });
     test('Above 1 fraction - default decimals - custom formatting - currency', () => {
-      expect(
-        toFixed(fractionAbove1Str, null, [
+      const opts = {
+        formatting: [
           'en-US',
           {
             style: 'currency',
             currency: 'USD',
           },
-        ]),
-      ).toEqual('$47,619.05');
+        ],
+      };
+      expect(toFixed(fractionAbove1Str, opts)).toEqual('$47,619.05');
     });
     test('Above 1 fraction - default decimals - custom formatting - currency and decimals', () => {
-      expect(
-        toFixed(fractionAbove1Str, null, [
+      const opts = {
+        formatting: [
           'en-US',
           { style: 'currency', currency: 'USD', maximumFractionDigits: 3 },
-        ]),
-      ).toEqual('$47,619.048');
+        ],
+      };
+      expect(toFixed(fractionAbove1Str, opts)).toEqual('$47,619.048');
     });
 
     test('Bellow 1 fraction - fixed default', () => {
       expect(toFixed(fractionBellow1Str)).toEqual('0.42857');
     });
     test('Bellow 1 fraction - 7 fixed', () => {
-      expect(toFixed(fractionBellow1Str, 7)).toEqual('0.4285714');
+      const opts = { decimalPlaces: 7 };
+      expect(toFixed(fractionBellow1Str, opts)).toEqual('0.4285714');
     });
     test('Bellow 1 fraction - 1 fixed', () => {
-      expect(toFixed(fractionBellow1Str, 1)).toEqual('0.4');
+      const opts = { decimalPlaces: 1 };
+      expect(toFixed(fractionBellow1Str, opts)).toEqual('0.4');
     });
     test('Bellow 1 fraction - 0 fixed', () => {
-      expect(toFixed(fractionBellow1Str, 0)).toEqual('0.42857');
+      const opts = { decimalPlaces: 0 };
+      expect(toFixed(fractionBellow1Str, opts)).toEqual('0.42857');
     });
   });
 
@@ -108,47 +124,54 @@ describe('Fraction Formatting', () => {
 
       describe(`${fixType} Input`, () => {
         test(`toAuto above 1 default - ${fixType}`, () => {
-          expect(toAuto(above1)).toEqual('47619.05');
+          expect(toAuto(above1)).toEqual('47619.04762');
         });
         test(`toAuto above 1 decimals 7 - ${fixType}`, () => {
-          expect(toAuto(above1, 7)).toEqual('47619.0476190');
+          const opts = { decimalPlaces: 7 };
+          expect(toAuto(above1, opts)).toEqual('47619.0476190');
         });
         test(`toAuto above 1 decimals 10 - ${fixType}`, () => {
-          expect(toAuto(above1, 10)).toEqual('47619.0476190476');
+          const opts = { decimalPlaces: 10 };
+          expect(toAuto(above1, opts)).toEqual('47619.0476190476');
         });
         test(`toAuto above 1 decimals 1 - ${fixType}`, () => {
-          expect(toAuto(above1, 1)).toEqual('47619.0');
+          const opts = { decimalPlaces: 1 };
+          expect(toAuto(above1, opts)).toEqual('47619.0');
         });
         test(`toAuto above 1 decimals 3 - ${fixType}`, () => {
-          expect(toAuto(above1, 3)).toEqual('47619.048');
+          const opts = { decimalPlaces: 3 };
+          expect(toAuto(above1, opts)).toEqual('47619.048');
         });
         test(`toAuto bellow 1 default - ${fixType}`, () => {
           expect(toAuto(bellow1)).toEqual('0.42857');
         });
         test(`toAuto bellow 1 decimals 7 - ${fixType}`, () => {
-          expect(toAuto(bellow1, 7)).toEqual('0.4285714');
+          const opts = { decimalPlaces: 7 };
+          expect(toAuto(bellow1, opts)).toEqual('0.4285714');
         });
         test(`toAuto bellow 1 decimals 1 - ${fixType}`, () => {
-          expect(toAuto(bellow1, 1)).toEqual('0.4');
+          const opts = { decimalPlaces: 1 };
+          expect(toAuto(bellow1, opts)).toEqual('0.4');
         });
         test(`toAuto bellow 1 decimals 0 - ${fixType}`, () => {
-          expect(toAuto(bellow1, 0)).toEqual('0.42857');
+          const opts = { decimalPlaces: 0 };
+          expect(toAuto(bellow1, opts)).toEqual('0.42857');
         });
 
         describe('formatting', () => {
           test('null decimals, default format', () => {
-            expect(toAuto(above1, null, true)).toEqual('47,619.05');
+            const opts = { formatting: true };
+            expect(toAuto(above1, opts)).toEqual('47,619.04762');
           });
           test('5 decimals, default format', () => {
-            expect(toAuto(above1, 5, true)).toEqual('47,619.04762');
+            const opts = { decimalPlaces: 2, formatting: true };
+            expect(toAuto(above1, opts)).toEqual('47,619.05');
           });
           test('null decimals, money format', () => {
-            expect(
-              toAuto(above1, null, [
-                'en-US',
-                { style: 'currency', currency: 'USD' },
-              ]),
-            ).toEqual('$47,619.05');
+            const opts = {
+              formatting: ['en-US', { style: 'currency', currency: 'USD' }],
+            };
+            expect(toAuto(above1, opts)).toEqual('$47,619.05');
           });
         });
       });
--- test/unit/token-formatting.test.js
+++ test/unit/token-formatting.test.js
@@ -12,27 +12,31 @@ describe('Token Formatting', () => {
       expect(tokenToSignificant(token18, 18)).toEqual('2083.3');
     });
     test('18 decimals 7 significant', () => {
-      expect(tokenToSignificant(token18, 18, 7)).toEqual('2083.279');
+      const opts = { decimalPlaces: 7 };
+      expect(tokenToSignificant(token18, 18, opts)).toEqual('2083.279');
     });
     test('18 decimals 10 significant', () => {
-      expect(tokenToSignificant(token18, 18, 10)).toEqual('2083.27897');
+      const opts = { decimalPlaces: 10 };
+      expect(tokenToSignificant(token18, 18, opts)).toEqual('2083.27897');
     });
     test('Small 18 decimals default', () => {
       expect(tokenToSignificant(token18Small, 18)).toEqual('0.27897');
     });
     test('Small 18 decimals 7 significant', () => {
-      expect(tokenToSignificant(token18Small, 18, 7)).toEqual('0.2789702');
+      const opts = { decimalPlaces: 7 };
+      expect(tokenToSignificant(token18Small, 18, opts)).toEqual('0.2789702');
     });
     test('Small 18 decimals 1 significant', () => {
-      expect(tokenToSignificant(token18Small, 18, 1)).toEqual('0.3');
+      const opts = { decimalPlaces: 1 };
+      expect(tokenToSignificant(token18Small, 18, opts)).toEqual('0.3');
     });
     test('Small 18 decimals 0 significant', () => {
-      expect(tokenToSignificant(token18Small, 18, 0)).toEqual('0.27897');
+      const opts = { decimalPlaces: 0 };
+      expect(tokenToSignificant(token18Small, 18, opts)).toEqual('0.27897');
     });
     test('Small 18 decimals default significant and custom formating', () => {
-      expect(tokenToSignificant(token18Small, 18, null, true)).toEqual(
-        '0.27897',
-      );
+      const opts = { formatting: true };
+      expect(tokenToSignificant(token18Small, 18, opts)).toEqual('0.27897');
     });
   });
   describe('toFixed', () => {
@@ -40,22 +44,27 @@ describe('Token Formatting', () => {
       expect(tokenToFixed(token18, 18)).toEqual('2083.27897');
     });
     test('18 decimals 7 fixed', () => {
-      expect(tokenToFixed(token18, 18, 7)).toEqual('2083.2789702');
+      const opts = { decimalPlaces: 7 };
+      expect(tokenToFixed(token18, 18, opts)).toEqual('2083.2789702');
     });
     test('18 decimals 10 fixed', () => {
-      expect(tokenToFixed(token18, 18, 10)).toEqual('2083.2789701517');
+      const opts = { decimalPlaces: 10 };
+      expect(tokenToFixed(token18, 18, opts)).toEqual('2083.2789701517');
     });
     test('Small 18 decimals fixed default', () => {
       expect(tokenToFixed(token18Small, 18)).toEqual('0.27897');
     });
     test('Small 18 decimals 7 fixed', () => {
-      expect(tokenToFixed(token18Small, 18, 7)).toEqual('0.2789702');
+      const opts = { decimalPlaces: 7 };
+      expect(tokenToFixed(token18Small, 18, opts)).toEqual('0.2789702');
     });
     test('Small 18 decimals 1 fixed', () => {
-      expect(tokenToFixed(token18Small, 18, 1)).toEqual('0.3');
+      const opts = { decimalPlaces: 1 };
+      expect(tokenToFixed(token18Small, 18, opts)).toEqual('0.3');
     });
     test('Small 18 decimals 0 fixed', () => {
-      expect(tokenToFixed(token18Small, 18, 0)).toEqual('0.27897');
+      const opts = { decimalPlaces: 0 };
+      expect(tokenToFixed(token18Small, 18, opts)).toEqual('0.27897');
     });
   });
   describe('toAuto', () => {
@@ -63,43 +72,50 @@ describe('Token Formatting', () => {
       expect(tokenToAuto(token18, 18)).toEqual('2083.28');
     });
     test('18 decimals 7 auto', () => {
-      expect(tokenToAuto(token18, 18, 7)).toEqual('2083.2789702');
+      const opts = { decimalPlaces: 7 };
+      expect(tokenToAuto(token18, 18, opts)).toEqual('2083.2789702');
     });
     test('18 decimals 10 auto', () => {
-      expect(tokenToAuto(token18, 18, 10)).toEqual('2083.2789701517');
+      const opts = { decimalPlaces: 10 };
+      expect(tokenToAuto(token18, 18, opts)).toEqual('2083.2789701517');
     });
     test('18 decimals 1 auto', () => {
-      expect(tokenToAuto(token18, 18, 1)).toEqual('2083.3');
+      const opts = { decimalPlaces: 1 };
+      expect(tokenToAuto(token18, 18, opts)).toEqual('2083.3');
     });
     test('18 decimals 3 auto', () => {
-      expect(tokenToAuto(token18, 18, 3)).toEqual('2083.279');
+      const opts = { decimalPlaces: 3 };
+      expect(tokenToAuto(token18, 18, opts)).toEqual('2083.279');
     });
     test('Small 18 decimals auto default', () => {
       expect(tokenToAuto(token18Small, 18)).toEqual('0.27897');
     });
     test('Small 18 decimals 7 auto', () => {
-      expect(tokenToAuto(token18Small, 18, 7)).toEqual('0.2789702');
+      const opts = { decimalPlaces: 7 };
+      expect(tokenToAuto(token18Small, 18, opts)).toEqual('0.2789702');
     });
     test('Small 18 decimals 1 auto', () => {
-      expect(tokenToAuto(token18Small, 18, 1)).toEqual('0.3');
+      const opts = { decimalPlaces: 1 };
+      expect(tokenToAuto(token18Small, 18, opts)).toEqual('0.3');
     });
     test('Small 18 decimals 0 auto', () => {
-      expect(tokenToAuto(token18Small, 18, 0)).toEqual('0.27897');
+      const opts = { decimalPlaces: 0 };
+      expect(tokenToAuto(token18Small, 18, opts)).toEqual('0.27897');
     });
     describe('formatting', () => {
       test('null decimals, default format', () => {
-        expect(tokenToAuto(token18, 18, null, true)).toEqual('2,083.28');
+        const opts = { formatting: true };
+        expect(tokenToAuto(token18, 18, opts)).toEqual('2,083.28');
       });
       test('5 decimals, default format', () => {
-        expect(tokenToAuto(token18, 18, 5, true)).toEqual('2,083.27897');
+        const opts = { formatting: true, decimalPlaces: 2 };
+        expect(tokenToAuto(token18, 18, opts)).toEqual('2,083.28');
       });
       test('null decimals, money format', () => {
-        expect(
-          tokenToAuto(token18, 18, null, [
-            'en-US',
-            { style: 'currency', currency: 'USD' },
-          ]),
-        ).toEqual('$2,083.28');
+        const opts = {
+          formatting: ['en-US', { style: 'currency', currency: 'USD' }],
+        };
+        expect(tokenToAuto(token18, 18, opts)).toEqual('$2,083.27897');
       });
     });
   });
